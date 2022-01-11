902d613d15890460ac020c8edd54f9c0d48b700b
Purushothaman Ganesan <71747442+purushothaman19@users.noreply.github.com>
2022-01-11 14:58:18 +0530

    added files via upload

---
+++ Tinder.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+E_MAIL = 'purushjames@gmail.com'
+PASSWORD = 'fubkoc-vongY6-hudfyh'
+
+chrome_driver_path = '/Users/purush/Development/chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+
+tinder_url = 'https://tinder.com/app/recs'
+driver.get(tinder_url)
+
+base_window = driver.window_handles[0]
+driver.switch_to.window(base_window)
+
+login = driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div/div/header/'
+                                     'div/div[2]/div[2]/button')
+login.click()
+
+try:
+    time.sleep(2)
+    facebook = driver.find_element_by_xpath('//*[@id="t--149300558"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+    # print(facebook.text)
+    facebook.click()
+
+except NoSuchElementException:
+    time.sleep(2)
+    more = driver.find_element_by_xpath('//*[@id="t--149300558"]/div/div/div[1]/div/div[3]/span/button')
+    more.click()
+
+time.sleep(1)
+facebook = driver.find_element_by_xpath('//*[@id="t--149300558"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+# print(facebook.text)
+facebook.click()
+
+fb_window = driver.window_handles[1]
+driver.switch_to.window(fb_window)
+
+email = driver.find_element_by_id('email')
+email.send_keys(E_MAIL)
+
+password = driver.find_element_by_id('pass')
+password.send_keys(PASSWORD)
+
+enter = driver.find_element_by_id('loginbutton')
+enter.click()
+
+time.sleep(2)
+continue_window = driver.window_handles[2]
+driver.switch_to.window(continue_window)
+continue_as_purush = driver.find_element_by_xpath('//*[@id="u_0_4_bI"]/div[2]/div[2]/div[1]/button')
+continue_as_purush.click()
